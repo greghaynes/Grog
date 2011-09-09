0ed9d14fd27188739b668f4481221e54c166b5df
Gregory Haynes <greg@greghaynes.net>
2011-09-09 01:02:19 -0700

    grog.get_entry caches retrieved entries

--- viewer/grog.js
+++ viewer/grog.js
@@ -1,11 +1,16 @@
 var grog = {
+	entries : {},
+
 	get_latest_entries: function(handler, offset, count) {
 		offset = typeof(offset) != 'undefined' ? offset: 0;
 		count = typeof(count) != 'undefined' ? count: 0;
 
 		if(count == 0) {
 			if(offset == 0)
-				$.getJSON('/entries/latest', handler);
+				$.getJSON('/entries/latest', function(data) {
+					$.each(data, function(ndx, entry) { grog.entries[entry.id] = entry; });
+					handler(data);
+					});
 			else
 				$.getJSON('/entries/latest/'+offset, handler);
 		} else
@@ -13,7 +18,10 @@ var grog = {
 	},
 
 	get_entry: function(entry_id, handler) {
-		$.getJSON('/entry/'+entry_id, handler);
+		if(entry_id in grog.entries)
+			handler(grog.entries[entry_id]);
+		else
+			$.getJSON('/entry/'+entry_id, handler);
 	}
 };
 
