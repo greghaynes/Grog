a04561c54fb2fe96640b478c1e2a6e9b28571556
Gregory Haynes <greg@greghaynes.net>
2012-11-26 00:06:05 -0800

    Split models, views, collections into separate namespaces. Added entry sorting

--- viewer/js/grog.js
+++ viewer/js/grog.js
@@ -1,84 +1,146 @@
 (function ($) {
 
 // Stub content for testing
-var stub_contacts = [
-    { fullname: "Fred Fredderson", username: 'fredf' },
+var stub_tags = [
+    { slug: 'tag1', name: 'Tag 1' },
+    { slug: 'tag2', name: 'Tag 2' }
 ];
 
 var stub_entries = [
     {
+        id: 0,
         slug: 'the_first_entry',
         title: "The First Entry",
         content: "This is the first entry, not a lot to <b>see</b> here.", 
-        tags: ['Tag1'],
-        creator: stub_contacts[0],
+        tags: [stub_tags[0]],
+        creator: 'testuser',
         created_on: "2008-07-17T09:24:17Z",
         modified_on: "2008-07-17T09:24:17Z",
     }, {
+        id: 1,
         slug: 'second_entry',
         title: "A Second Entry",
         content: "This is the second entry. There is not a lot to <b>see</b> here.", 
-        tags: ['Tag1', 'Tag2'],
-        creator: stub_contacts[0],
+        tags: [stub_tags[0], stub_tags[1]],
+        creator: 'testuser',
+        comments: [
+            {
+                title: 'Comment one',
+                content: 'This it comment number one',
+                author: 'Some person'
+            },
+        ],
     },
 ];
 
-var Author = Backbone.Model.extend({
-    defaults: {
-        fullname: 'John Doe',
-        username: 'someuser',
-    }
-})
+var Models = {
+    Author: Backbone.Model.extend({
+        urlRoot: '/users/',
+        defaults: {
+            fullname: 'John Doe',
+            username: 'someuser',
+        },
+    }),
 
-var Entry = Backbone.Model.extend({
-    defaults: {
-        slug: 'invalid_slug',
-        title: 'No title',
-        content: 'No content.',
-        tags: [],
-        created_on: new Date().toISOString(),
-        modified_on: new Date().toISOString(),
-        creator: new Author(),
-    }
-});
+    EntryTag: Backbone.Model.extend({
+        defaults: {
+            slug: 'invalid_slug',
+            name: 'Unknown Tag',
+            description: 'Not loaded',
+        }
+    }),
 
-var Entries = Backbone.Collection.extend({
-    model: Entry
-});
+    Comment: Backbone.Model.extend({
+        defaults: {
+            id: -1,
+            title: 'Unknown comment',
+            content: 'No content.',
+            author: 'Unknown author',
+        }
+    }),
 
-var EntryView = Backbone.View.extend({
-    tagName: "article",
-    className: "entry-container",
-    template: Handlebars.compile($("#entryTemplate").html()),
-    render: function() {
-        var ctxt = this.model.toJSON();
-        ctxt['created_timeago'] = $.timeago(ctxt['created_on']);
-        console.log(ctxt, "Render entry");
-        this.$el.html(this.template(ctxt));
-        return this;
-    }
-});
+    Entry: Backbone.Model.extend({
+        defaults: {
+            id: -1,
+            slug: 'invalid_slug',
+            title: 'No title',
+            content: 'No content.',
+            tags: [],
+            created_on: new Date().toISOString(),
+            modified_on: new Date().toISOString(),
+            creator: '',
+            comments: [],
+        }
+    }),
+};
 
-var EntriesView = Backbone.View.extend({
-    el: $("#entries"),
-    initialize: function() {
-        this.collection = new Entries(stub_entries);
-        this.render();
-    },
-    render: function() {
-        var view_ctxt = this;
-        _.each(this.collection.models, function(item) {
-            view_ctxt.renderEntry(item);
-        }, this);
-    },
-    renderEntry: function(entry) {
-        var entryView = new EntryView({
-            model: entry,
-        });
-        this.$el.append(entryView.render().el);
-    }
-});
+var Collections = {
+    Authors: Backbone.Collection.extend({
+        model: Models.Author
+    }),
 
-var entries_view = new EntriesView();
+    Entries: Backbone.Collection.extend({
+        model: Models.Entry,
+        comparator: function(entry) {
+            return - new Date(entry.get('created_on')).valueOf();
+        }
+    }),
+};
+
+var Views = {
+    EntryAuthorView: Backbone.View.extend({
+        template: Handlebars.compile($("#entryAuthorTemplate").html()),
+        initialize: function() {
+            this.model.on('change', this.render, this);
+        },
+        render: function() {
+            console.log(this.model.toJSON(), 'rendering author');
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    }),
+
+    EntryView: Backbone.View.extend({
+        tagName: "article",
+        className: "entry-container",
+        template: Handlebars.compile($("#entryTemplate").html()),
+        render: function() {
+            var ctxt = this.model.toJSON();
+            ctxt['created_timeago'] = $.timeago(ctxt['created_on']);
+            console.log(ctxt, "Render entry");
+            this.$el.html(this.template(ctxt));
+            
+            var author = new Models.Author({id: ctxt['creator']});
+            author.fetch();
+            var authorView = new Views.EntryAuthorView({
+                el: this.$('#entry-author-'+ctxt['slug']),
+                model: author
+            });
+            return this;
+        }
+    }),
+
+    EntriesView: Backbone.View.extend({
+        el: $("#entries"),
+        initialize: function() {
+            this.collection = new Collections.Entries(stub_entries);
+            this.render();
+        },
+        render: function() {
+            var view_ctxt = this;
+            _.each(this.collection.models, function(item) {
+                view_ctxt.renderEntry(item);
+            }, this);
+        },
+        renderEntry: function(entry) {
+            var entryView = new Views.EntryView({
+                model: entry,
+            });
+            this.$el.append(entryView.render().el);
+        }
+    }),
+};
+
+var entries_view = new Views.EntriesView();
 
 } (jQuery));
